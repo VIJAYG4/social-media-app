73ef1dc716b5c408cf547a9b94e3c71e37674300
VIJAYG4 <vijayg.ece.4@gmail.com>
2020-11-25 13:52:06 -0500

    create and delete comments, like/unlike posts, pubsub for new post notification

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -47,11 +47,10 @@ module.exports = {
           });
     
           const post = await newPost.save();
-    
-        //   context.pubsub.publish('NEW_POST', {
-        //     newPost: post
-        //   });
-    
+          
+          context.pubsub.publish("NEW_POST",{
+              newPost: post
+          })
           return post;
         },
 
@@ -69,6 +68,35 @@ module.exports = {
             }catch(err){
                 throw new Error(err);
             }
+        },
+        async likePost(_, {postId}, context){
+            const { username } = checkAuth(context);
+            const post = await Post.findById(postId);
+    
+            if(post){
+              if(post.likes.find((like) => like.username === username)){
+                //Post already liked, unlike it  
+                post.likes = post.likes.filter((like) => like.username !== username);
+              }else{
+                //Not liked, like the post
+                post.likes.push({
+                  username,
+                  createdAt: new Date().toISOString()
+                });
+              }
+              //save the post and return it
+              await post.save();
+              return post;
+            }else throw new UserInputError("Post doens't exist");
+    
+            
+          } 
+    },
+
+    Subscription: {
+        newPost:{
+            subscribe: (_,__, { pubsub }) => pubsub.asyncIterator("NEW_POST")
         }
+    
     }
 }
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config');
 
-
+const pubsub = new PubSub();
 
 //create server instance
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req, pubsub })
 });
 
 //connect to MONGODB and start the server - uses express server behind the scenes
